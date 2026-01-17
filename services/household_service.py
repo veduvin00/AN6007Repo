7620fcf7543beda885e5f4fe7ff7345a1f8a3e73
liurunlin0818 <liurunlin0818@outlook.com>
2026-01-17 22:14:23 +0800

    1

--- services/household_service.py
+++ services/household_service.py
@@ -1,58 +1,61 @@
 import json
 import os
+import models.household as Household
 from utils.id_generator import generate_household_id
 
 HOUSEHOLD_FILE = "storage/households.json"
 households = {}
 
 def load_households():
+
     global households
-
-    if not os.path.exists(HOUSEHOLD_FILE):
-        households = {}
-        return
-
-    if os.path.getsize(HOUSEHOLD_FILE) == 0:
+    if not os.path.exists(HOUSEHOLD_FILE) or os.path.getsize(HOUSEHOLD_FILE) == 0:
         households = {}
         return
 
     with open(HOUSEHOLD_FILE, "r") as f:
-        households = json.load(f)
+        data = json.load(f)
+        households = {hid: Household.from_dict(hdata) for hid, hdata in data.items()}
 
 def save_households():
     os.makedirs("storage", exist_ok=True)
     with open(HOUSEHOLD_FILE, "w") as f:
-        json.dump(households, f, indent=2)
+        json_data = {hid: h.to_dict() for hid, h in households.items()}
+        json.dump(json_data, f, indent=2)
 
 def register_household(data):
-    if not data:
-        return {"error": "Invalid JSON body"}, 400
-
-    if "members" not in data or "postal_code" not in data:
+    if not data or "members" not in data or "postal_code" not in data:
         return {"error": "Missing required fields"}, 400
 
     household_id = generate_household_id()
     while household_id in households:
         household_id = generate_household_id()
 
-    households[household_id] = {
-        "members": data["members"],
-        "postal_code": data["postal_code"],
-        "vouchers": {}
-    }
+    new_household = Household(
+        household_id = household_id,
+        members = data["members"],
+        postal_code = data["postal_code"]
+    )
 
+    households[household_id] = new_household
     save_households()
+
+    claim_link = f"http://127.0.0.1/api/households/{household_id}/claim"
 
     return {
         "message": "Household registered successfully",
-        "household_id": household_id
+        "household_id": household_id,
+        "claim_link": claim_link
     }, 201
 
 def get_redemption_balance(household_id):
     if household_id not in households:
         return {"error": "Household not found"}, 404
 
+    household = households[household_id]
+
     return {
         "household_id": household_id,
-        "balance": households[household_id]["vouchers"]
+        "total_balance": household.get_total_balance(),
+        "vouchers": household.vouchers
     }, 200
