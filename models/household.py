a5842e4fce292f86bd8d59425f26db38f3a315ac
liurunlin0818 <liurunlin0818@outlook.com>
2026-01-17 22:02:20 +0800

    Update: claim_tranche; get_total_balance

--- models/household.py
+++ models/household.py
@@ -1,9 +1,31 @@
 class Household:
-    def __init__(self, household_id, members, postal_code):
+
+    TRANCHE_CONFIG = {
+        "May2025":{2: 50, 5: 20, 10: 30},
+        "Jan2026":{2: 30, 5: 20, 10: 14}
+    }
+
+    def __init__(self, household_id, members, postal_code, vouchers = None):
         self.household_id = household_id
         self.members = members
         self.postal_code = postal_code
-        self.vouchers = {}  # tranche -> denomination map
+        self.vouchers = vouchers if vouchers is not None else {}
+
+    def claim_tranche(self, tranche_name):
+        if tranche_name not in self.TRANCHE_CONFIG:
+            return False, "Invalid tranche name."
+        if tranche_name in self.vouchers:
+            return False, "Tranche already claimed."
+        
+        self.vouchers[tranche_name] = self.TRANCHE_CONFIG[tranche_name].copy()
+        return True, "Vouchers claimed successfully."
+
+    def get_total_balance(self):
+        total = 0
+        for tranche in self.vouchers.values():
+            for denomination, count in tranche.items():
+                total += int(denomination) * count
+        return total
 
     def to_dict(self):
         return {
@@ -12,3 +34,12 @@ class Household:
             "postal_code": self.postal_code,
             "vouchers": self.vouchers
         }
+    
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            household_id = data["household_id"],
+            members = data["members"],
+            postal_code = data["postal_code"],
+            vouchers = data.get("vouchers", {})
+        )
